b7ee17e264d2c518448b6c9164462bc493710fa7
Ramon Saraiva <ramonsaraiva@gmail.com>
2014-06-15 23:10:38 -0300

    removed vnc old stuff

--- vayne.py
+++ vayne.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from connection import Connection
-from vnc_expl import newb_expl
 from threading import Thread
-import json
-import random
 
 from leagueoflegends import LeagueOfLegends
 from leagueoflegends import RiotError
@@ -29,7 +26,6 @@ class Bot:
 		self.__vnc = {}
 		self.__key = 0
 		self.__nick = nick
-		self.load_masks()
 
 	def add_connection(self, sv, port):
 		con = Connection(sv, port)
@@ -80,8 +76,7 @@ class Bot:
 
 		if text:
 			if text[0] == self.PREFIX and len(text.split(' ')) >= 2:
-				print('CMD!')
-				self.parse_cmd(text)
+				self.parse_cmd(text, source)
 
 	def check_args(self, args, req):
 		if len(args) < req:
@@ -89,24 +84,22 @@ class Bot:
 			return False
 		return True
 
+	def check_owner(self, source):
+		if source in self.OWNERS:
+			return True
+		self.printayne('who the fuck are you?', self.__channels)
+		return False
+
 	def add_jobs(self, key, jobs):
 		if key not in self.__threads:
 			self.__threads[key] = []
-		for job in jobs:	
+		for job in jobs:
 			self.__threads[key].append(job)
 
-	def load_masks(self):
-		vncf = open('vnc.json', 'r')
-		vncj = json.load(vncf)
-		self.__vnc['masks'] = vncj['masks']
-		vncf.close()
+	def lolit(self):
+		return LeagueOfLegends(self.RIOT_API_KEY)
 
-	def update_masks(self):
-		vncf = open('vnc.json', 'w')
-		vncf.write(json.dumps(self.__vnc))
-		vncf.close()
-
-	def parse_cmd(self, cmd):
+	def parse_cmd(self, cmd, source):
 		args = cmd[2:].split(' ')
 		if len(args) > 1:
 			cmd, args = cmd[2:].split(' ', 1)
@@ -114,29 +107,22 @@ class Bot:
 			cmd = args[0]
 			args = ''
 		args = args.split(' ')
+		source = source.split('!')[0]
 
 		if (cmd == 'nick'):
+			if not self.check_owner(source):
+				return
 			if not self.check_args(args, 1):
 				return
 
 			self.currcon.write(('NICK', args[0]))
 		if (cmd == 'join'):
+			if not self.check_owner(source):
+				return
 			if not self.check_args(args, 1):
 				return
 			self.currcon.write(('JOIN', args[0]))
-		if (cmd == 'vnc_newb'):
-			if not self.check_args(args, 1):
-				return
-			self.vnc_newb_expl_div(args[0])
-			self.printayne('5 jobs now exploiting the mask {0}'.format(args[0]), self.__channels)
-		if (cmd == 'vnc_global'):
-			t = Thread(target=self.vnc_global, args=(args[0] == 'clean',))
-			self.add_jobs('vnc', [t])
-			t.start()
-			self.printayne('global vnc scan initialized', self.__channels)
-			self.update_masks()
-		if (cmd == 'masks'):
-			self.printayne('masks => {0}'.format(' - '.join(self.__vnc['masks'])), self.__channels)
+
 		if (cmd == 'jobs'):
 			ct = 0
 			jobs = ''
@@ -145,6 +131,7 @@ class Bot:
 				ct += l
 				jobs += '{0}[{1}] '.format(k, l)
 			self.printayne('currently running {0} jobs => {1}'.format(ct, jobs), self.__channels)
+
 		if (cmd == 'stop'):
 			if not self.check_args(args, 1):
 				return
@@ -153,6 +140,7 @@ class Bot:
 				return
 			if args[0] in self.__threads:
 				del self.__threads[args[0]][:]
+
 		if (cmd == 'summoner'):
 			if not self.check_args(args, 2):
 				return
@@ -170,7 +158,7 @@ class Bot:
 			except RiotError, e:
 				self.printayne('riot error => {0}'.format(e.error_msg), self.__channels)
 
-		if (cmd == 'soloq'):
+		if (cmd == 'rank'):
 			if not self.check_args(args, 2):
 				return
 
@@ -196,49 +184,6 @@ class Bot:
 				
 			except RiotError, e:
 				self.printayne('riot error => {0}'.format(e.error_msg), self.__channels)
-
-	def lolit(self):
-		return LeagueOfLegends(self.RIOT_API_KEY)
-
-	def random_mask(self):
-		r_first = random.randint(0, 255)
-		r_second = random.randint(0, 255)
-		mask = '{0}.{1}'.format(r_first, r_second)
-		return [r_first, r_second, mask]
-
-	def vnc_newb_expl(self, mask, b, e):
-		for i in xrange(b, e):
-			for j in range(256):
-				if ('vnc' in self.__threads and not self.__threads['vnc']) or 'vnc' not in self.__threads:
-					return
-				host = '{0}.{1}.{2}'.format(mask, i, j)
-				if newb_expl(host):
-					self.printayne('vnc vulnerable => {0}'.format(host), self.__channels)
-
-	def vnc_newb_expl_div(self, mask, join=False):
-		threads = []
-		for i in range(5):
-			b = i * 51
-			e = b + 51
-			threads.append(Thread(target=self.vnc_newb_expl, args=(mask, b, e)))
-		
-		[t.start() for t in threads]
-		self.add_jobs('vnc', threads)
-		if join:
-			[t.join() for t in threads]
-
-	def vnc_global(self, clean):
-		if clean:
-			self.__vnc['masks'] = []
-		r_first, r_second, mask = self.random_mask()
-		while True:
-			if ('vnc' in self.__threads and not self.__threads['vnc']) or 'vnc' not in self.__threads:
-					return
-			#subnetworks in 192.168.x.x or between 172.16 and 172.31 are private so no need for scanning
-			while mask in self.__vnc['masks'] or r_first == 10 or mask == '192.168' or (r_first == 172 and r_second >= 16 and r_second <= 31):
-				r_first, r_second, mask = self.random_mask()
-			self.vnc_newb_expl_div(mask, True)
-			self.__vnc['masks'].append(mask)
 
 	def work(self):
 		while True:
