7d55bbdf8635771117367433e627bd8830bb6c26
Ramon Saraiva <ramonsaraiva@gmail.com>
2014-06-16 02:26:20 -0300

    added last match data

--- vayne.py
+++ vayne.py
@@ -18,6 +18,163 @@ class Bot:
 		'RANKED_SOLO_5x5': 'solo',
 	}
 
+	K = {
+		#game
+		'mode': 'gameMode', #classic
+		'stype': 'subType', #normal, ranked5x5
+		's1': 'spell1',
+		's2': 'spell2',
+		#game stats
+		'win': 'win',
+		'lvl': 'level',
+		'gold': 'goldEarned',
+		'spent': 'goldSpent',
+		'cs': 'minionsKilled',
+		'jcs': 'neutralMinionsKilled',
+		'k': 'championsKilled',
+		'd': 'numDeaths',
+		'a': 'assists',
+	}
+
+	CHAMPIONS = {
+		'1': 'Annie',
+		'2': 'Olaf',
+		'3': 'Galio',
+		'4': 'TwistedFate',
+		'5': 'XinZhao',
+		'6': 'Urgot',
+		'7': 'Leblanc',
+		'8': 'Vladimir',
+		'9': 'FiddleSticks',
+		'10': 'Kayle',
+		'11': 'MasterYi',
+		'12': 'Alistar',
+		'13': 'Ryze',
+		'14': 'Sion',
+		'15': 'Sivir',
+		'16': 'Soraka',
+		'17': 'Teemo',
+		'18': 'Tristana',
+		'19': 'Warwick',
+		'20': 'Nunu',
+		'21': 'MissFortune',
+		'22': 'Ashe',
+		'23': 'Tryndamere',
+		'24': 'Jax',
+		'25': 'Morgana',
+		'26': 'Zilean',
+		'27': 'Singed',
+		'28': 'Evelynn',
+		'29': 'Twitch',
+		'30': 'Karthus',
+		'31': 'Chogath',
+		'32': 'Amumu',
+		'33': 'Rammus',
+		'34': 'Anivia',
+		'35': 'Shaco',
+		'36': 'DrMundo',
+		'37': 'Sona',
+		'38': 'Kassadin',
+		'39': 'Irelia',
+		'40': 'Janna',
+		'41': 'Gangplank',
+		'42': 'Corki',
+		'43': 'Karma',
+		'44': 'Taric',
+		'45': 'Veigar',
+		'48': 'Trundle',
+		'50': 'Swain',
+		'51': 'Caitlyn',
+		'53': 'Blitzcrank',
+		'54': 'Malphite',
+		'55': 'Katarina',
+		'56': 'Nocturne',
+		'57': 'Maokai',
+		'58': 'Renekton',
+		'59': 'JarvanIV',
+		'60': 'Elise',
+		'61': 'Orianna',
+		'62': 'MonkeyKing',
+		'63': 'Brand',
+		'64': 'LeeSin',
+		'67': 'Vayne',
+		'68': 'Rumble',
+		'69': 'Cassiopeia',
+		'72': 'Skarner',
+		'74': 'Heimerdinger',
+		'75': 'Nasus',
+		'76': 'Nidalee',
+		'77': 'Udyr',
+		'78': 'Poppy',
+		'79': 'Gragas',
+		'80': 'Pantheon',
+		'81': 'Ezreal',
+		'82': 'Mordekaiser',
+		'83': 'Yorick',
+		'84': 'Akali',
+		'85': 'Kennen',
+		'86': 'Garen',
+		'89': 'Leona',
+		'90': 'Malzahar',
+		'91': 'Talon',
+		'92': 'Riven',
+		'96': 'KogMaw',
+		'98': 'Shen',
+		'99': 'Lux',
+		'101': 'Xerath',
+		'102': 'Shyvana',
+		'103': 'Ahri',
+		'104': 'Graves',
+		'105': 'Fizz',
+		'106': 'Volibear',
+		'107': 'Rengar',
+		'110': 'Varus',
+		'111': 'Nautilus',
+		'112': 'Viktor',
+		'113': 'Sejuani',
+		'114': 'Fiora',
+		'115': 'Ziggs',
+		'117': 'Lulu',
+		'119': 'Draven',
+		'120': 'Hecarim',
+		'121': 'Khazix',
+		'122': 'Darius',
+		'126': 'Jayce',
+		'127': 'Lissandra',
+		'131': 'Diana',
+		'133': 'Quinn',
+		'134': 'Syndra',
+		'143': 'Zyra',
+		'154': 'Zac',
+		'157': 'Yasuo',
+		'161': 'Velkoz',
+		'201': 'Braum',
+		'222': 'Jinx',
+		'236': 'Lucian',
+		'238': 'Zed',
+		'254': 'Vi',
+		'266': 'Aatrox',
+		'267': 'Nami',
+		'412': 'Thresh',
+	}
+
+	SPELLS = {
+		'11': 'Smite',
+		'10': 'Revive',
+		'13': 'Clarity',
+		'12': 'Teleport',
+		'21': 'Barrier',
+		'17': 'Garrison',
+		'1': 'Cleanse',
+		'3': 'Exhaust',
+		'2': 'Clairvoyance',
+		'4': 'Flash',
+		'7': 'Heal',
+		'6': 'Ghost',
+		'14': 'Ignite',
+	}
+
+
 	def __init__(self, nick):
 		self.__connections = []
 		self.__buffers = []
@@ -184,6 +341,37 @@ class Bot:
 				
 			except RiotError, e:
 				self.printayne('riot error => {0}'.format(e.error_msg), self.__channels)
+		
+		if (cmd == 'last'):
+			if not self.check_args(args, 2):
+				return
+
+			lol = self.lolit()
+			lol.set_api_region(args[1])
+			try:
+				lol.set_summoner(args[0])
+				s = lol.get_summoner_games()
+				last = s[0]
+				stats = last['stats']
+				l_win = 'WON' if stats[self.K['win']] else 'LOST'
+				l_champ = str(last['championId'])
+				l_type = last[self.K['stype']]
+				l_lvl = stats[self.K['lvl']]
+				l_spell1 = str(last[self.K['s1']])
+				l_spell2 = str(last[self.K['s2']])
+				l_kills = stats[self.K['k']]
+				l_deaths = stats[self.K['d']]
+				l_assists = 0
+				if self.K['a'] in stats:
+					l_assists = stats[self.K['a']]
+				l_kda = (float(l_kills) + float(l_deaths)) / float(l_assists)
+				l_cs = stats[self.K['cs']]
+				l_gold = stats[self.K['gold']]
+
+				self.printayne('{0} {1} his last game ({2}) as Lv. {3} {4} running {5}/{6}. KDA: {7}/{8}/{9} ({10:.2f}) CS: {11}, Total gold: {12}.'.format(args[0], l_win, l_type, l_lvl, self.CHAMPIONS[l_champ], self.SPELLS[l_spell1], self.SPELLS[l_spell2], l_kills, l_deaths, l_assists, l_kda, l_cs, l_gold), self.__channels)
+
+			except RiotError, e:
+				print self.printayne('riot error => {0}'.format(e.error_msg), self.__channels)
 
 	def work(self):
 		while True:
@@ -199,6 +387,7 @@ class Bot:
 
 if __name__ == '__main__':
 	bot = Bot('vayneluwl')
-	bot.add_connection('google.root-network.org', 6667)
-	bot.set_channels(['#vayne'])
+	#bot.add_connection('google.root-network.org', 6667)
+	bot.add_connection('irc.quakenet.org', 6667)
+	bot.set_channels(['#leagueoflegends'])
 	bot.connect()
